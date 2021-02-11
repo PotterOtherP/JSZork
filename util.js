e6be935555b5e705506d6cf6e424fff9d768141f
Nate Tryon <natetryon@gmail.com>
2021-02-10 20:14:37 -0500

    Delete util.js

--- util.js
+++
@@ -1,9 +0,0 @@
-function mobile_menu_open()
-{
-    document.getElementById("mobile-menu").style.display = "block";
-}
-
-function mobile_menu_close()
-{
-    document.getElementById("mobile-menu").style.display = "none";
-}
